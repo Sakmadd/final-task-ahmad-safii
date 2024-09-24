1c4526504e3962dcc4ac763310cf82ac53e7ff5e
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-24 21:46:07 +0700

    Question 4B : Adding customs utilities

---
+++ 4/4B/src/js/customUtility.js
@@ -0,0 +1,31 @@
+const newUtilities = {
+  
+  '.text-berani': {
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)',
+  },
+  '.input-style': {
+    width: '100%',
+    padding: '15px',
+    marginBottom: '10px',
+    border: '1px solid #ccc',
+    borderRadius: '50px',
+    fontSize: '16px',
+    color: '#333',
+    boxSizing: 'border-box',
+  },
+  '.button-style': {
+    width: '100%',
+    padding: '10px',
+    border: 'none',
+    borderRadius: '25px',
+    fontSize: '16px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+    color: '#fff',
+    boxSizing: 'border-box',
+    backgroundColor: 'rgb(209, 250, 213)',
+  },
+}
+
+
+module.exports = {newUtilities}
